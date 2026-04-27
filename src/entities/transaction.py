9e5f59202a65896f95fd30d740205b265fcc0361
Jaakko Roivas <108202147+Jaqt@users.noreply.github.com>
2026-04-28 00:32:12 +0300

    add docstring

--- src/entities/transaction.py
+++ src/entities/transaction.py
@@ -3,6 +3,24 @@ from dataclasses import dataclass
 
 @dataclass
 class Transaction:
+    """Luokka, joka kuvaa yksittäistä tulo- tai menotapahtumaa
+
+    Attributes:
+        user_id: Kokonaisluku, joka kuvaa tapahtuman omistavan käyttäjän tunnistetta
+            tietokannassa.
+        year: Kokonaisluku, joka kuvaa tapahtuman vuotta.
+        month: Kokonaisluku, joka kuvaa tapahtuman kuukautta.
+        transaction_type: Merkkijonoarvo, joka kuvaa tapahtuman tyyppiä (tulo tai meno).
+        category: Merkkijonoarvo, joka kuvaa tapahtuman kategoriaa.
+        amount: Liukuluku, joka kuvaa tapahtuman rahamäärää.
+        description:
+            Merkkijonoarvo, joka kuvaa tapahtuman kuvausta.
+            Oletuksena tyhjä merkkijono.
+        id: Kokonaisluku, joka kuvaa tapahtuman yksikäsitteistä tunnistetta
+            tietokannassa. Oletuksena None, jolloin tietokantaan tallennettaessa
+            luodaan uusi tunniste.
+    """
+
     user_id: int
     year: int
     month: int
--- src/entities/user.py
+++ src/entities/user.py
@@ -1,5 +1,24 @@
 class User:
+    """Luokka, joka kuvaa yksittäistä käyttäjää.
+
+    Attributes:
+        id: Kokonaisluku, joka kuvaa käyttäjän yksikäsitteistä tunnistetta tietokannassa.
+        username: Merkkijonoarvo, joka kuvaa käyttäjän käyttäjätunnusta.
+        password: Merkkijonoarvo, joka kuvaa käyttäjän salasanaa.
+    """
+
     def __init__(self, username, password, user_id=None):
+        """Luokan konstruktori, joka luo uuden käyttäjäolion.
+
+        Args:
+            username: Merkkijonoarvo, joka kuvaa käyttäjän käyttäjätunnusta.
+            password: Merkkijonoarvo, joka kuvaa käyttäjän salasanaa.
+            user_id:
+                Kokonaisluku, joka kuvaa käyttäjän yksikäsitteistä tunnistetta
+                tietokannassa. Oletuksena None, jolloin tietokantaan tallennettaessa
+                luodaan uusi tunniste.
+        """
+
         self.id = user_id
         self.username = username
         self.password = password
